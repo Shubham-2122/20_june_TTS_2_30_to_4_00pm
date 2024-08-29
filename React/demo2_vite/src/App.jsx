550e0724d6ecd1cc5591994c43ead73e02ed1136
shubham-2122 <shubhamjadav.tops@gmail.com>
2024-08-29 15:17:31 +0530

    code

--- React/demo2_vite/src/App.jsx
+++ React/demo2_vite/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react"
+import Demo from "./demo"
 
 function App() {
-
 
   return (
     <>
       <h1>hello this is a vite react</h1>
+      <Demo />
     </>
   )
 }
---
+++ React/demo2_vite/src/Demo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Demo() {
+  return (
+    <div>
+      <h1>helo this demo func</h1>
+    </div>
+  )
+}
+
+export default Demo;
